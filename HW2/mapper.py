639a31fcf013d25d79a5b713a2c91f9679806aea
Glenn D Dunmire IV <dunmireg@Glenns-Air.home>
2016-01-24 22:10:03 -0500

    adding 2.4

--- HW2/mapper.py
+++ HW2/mapper.py
@@ -4,11 +4,50 @@
 #produce ID + \t + word + \t + true spam flag to send to the reducer. 
 import sys
 import re
+import os 
+from math import log
+from math import exp
+
+priorSpam = 0
+priorHam = 0
+words = {}
+
+with open(os.path.join('./enroneEmailCondProbs', 'part-00000'), 'r') as myfile:
+    lines = myfile.readlines()
+    priorSpam = float(lines[0])
+    priorHam = float(lines[1])
+    for line in lines[2:]:
+        components = line.split('\t')
+        words[components[0]] = {'spam_like': float(components[1]), 'ham_like': float(components[2])}
+        
+
 WORD_RE = re.compile(r"[\w']+")
+
+spamSkip = 0
+hamSkip = 0
 
 for line in sys.stdin:
     components = line.split('\t')
     text = " ".join(components[-2:]).strip()
-    words = re.findall(WORD_RE, text)
-    for word in words:
-        print components[0] + '\t' + word + '\t' + components[1]
+    text = re.findall(WORD_RE, text)
+    
+    spamScore = log(priorSpam)
+    hamScore = log(priorHam)
+    for word in text:
+        if word in words.keys():
+            if float(words[word]['spam_like']) != 0:
+                spamScore += log(float(words[word]['spam_like']))
+            else:
+                spamScore += -300
+                spamSkip += 1
+            if float(words[word]['ham_like']) != 0:
+                hamScore += log(float(words[word]['ham_like']))
+            else:
+                hamScore += -300
+                hamSkip += 1
+        pred = 0
+    if spamScore > hamScore:
+        pred = 1
+    print components[0] + '\t' + components[1] + '\t' + str(pred) + '\t' + str(exp(spamScore)) + '\t' + str(exp(hamScore))
+print spamSkip
+print hamSkip
--- HW2/reducer.py
+++ HW2/reducer.py
@@ -1,62 +1,15 @@
 #!/usr/bin/python
 import sys
-import os
-from math import log
-from math import exp
 
-priorSpam = 0
-priorHam = 0
-words = {}
-
-with open(os.path.join('./enroneEmailCondProbs', 'part-00000'), 'r') as myfile:
-    lines = myfile.readlines()
-    priorSpam = float(lines[0])
-    priorHam = float(lines[1])
-    for line in lines[2:]:
-        components = line.split('\t')
-        words[components[0]] = {'spam_like': float(components[1]), 'ham_like': float(components[2])}
-
-        
-curID = None
-spamSkip = 0
-hamSkip = 0
-spamScore = log(priorSpam)
-hamScore = log(priorHam)
-curflag = 0
+misclassified = 0
 
 for line in sys.stdin:
     components = line.split('\t')
-    ID = components[0]
-    word = components[1]
-    flag = int(components[2])
-    
-    if curID == ID:
-        if word in words.keys():
-            if float(words[word]['spam_like']) != 0:
-                spamScore += log(float(words[word]['spam_like']))
-            else:
-                spamScore += -300
-                spamSkip += 1
-            if float(words[word]['ham_like']) != 0:
-                hamScore += log(float(words[word]['ham_like']))
-            else:
-                hamScore += -300
-                hamSkip += 1
-#             if float(words[word]['spam_like']) != 0.0 and float(words[word]['ham_like']) != 0.0:
-#                 spamScore += log(float(words[word]['spam_like']))
-#                 hamScore += log(float(words[word]['ham_like']))
+    if len(components) != 1:
+        if int(components[1]) != int(components[2]):
+                misclassified += 1
+        print line
     else:
-        if curID:
-            classification = 0
-            if spamScore > hamScore:
-                classification = 1
-            print curID + '\t' + str(flag) + '\t' + str(classification) + '\t' + str(exp(spamScore)) + '\t' + str(exp(hamScore))
-        curID = ID
-        curflag = flag
-        spamScore = log(priorSpam)
-        hamScore = log(priorHam)
+        print line
+print "Misclassified: " + str(misclassified) + " which means this has an accuracy of " + str(100-misclassified) + "%"
     
-if ID == curID:
-    print curID + '\t' + str(flag) + '\t' + str(classification) + '\t' + str(exp(spamScore)) + '\t' + str(exp(hamScore))
-    print spamSkip
-    print hamSkip
